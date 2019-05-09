3ffe1ef7f0cddf47344a7e4caa172dca1b9958b6
Noah <burghard@ualberta.ca>
2019-05-09 10:32:00 -0600

    added more LINQ examples

--- LINQ/LINQApp/LINQApp/CommentSearcher.cs
+++ LINQ/LINQApp/LINQApp/CommentSearcher.cs
@@ -50,13 +50,93 @@ namespace LINQApp
         {
             return ((JObject)this.comments["comments"][0]).Properties().Select(p => p.Name).ToList();
         }
-       
-        // TODO: fix dis
+
+        /// <summary>
+        /// Finds comments containing the given substring (including as part of a word).
+        /// Using query syntax.
+        /// </summary>
+        /// <param name="term">The substring to search for</param>
+        /// <returns>IEnumerable<> of comment body strings</returns>
         public IEnumerable<String> stringSearch(String term)
         {
-            return from c in this.comments["commnets"]
+            return from c in this.comments["comments"]
                    where ((String) c["body"]).Contains(term)
                    select (String) c["body"];
         }
+
+        // TODO: finds the most controversial comment
+        public String controversialComment()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Finds comments from the given subreddit, using method syntax.
+        /// </summary>
+        /// <param name="subreddit">Subreddit to get results from. Capitalization is important.</param>
+        /// <returns>IEnumerable of comment body strings</returns>
+        public IEnumerable<String> fromSub(String subreddit)
+        {
+            return this.comments["comments"]
+                .Where(c => ((String) c["subreddit"]) == subreddit)
+                .Select(c => (String) c["body"]);
+        }
+
+        /// <summary>
+        /// Returns number of short comments (under 20 characters), using lambda function assigned to
+        /// Func delegate.
+        /// </summary>
+        /// <returns>Integer number of short comments</returns>
+        public int getNumShortComments()
+        {
+            // Create lambda functions
+            Func<JToken, String> commentBody = c => ((String)c["body"]);
+            Func<JToken, bool> isShort = c => (commentBody(c)).Length <= 20;
+
+            // Parse data
+            IEnumerable<String> shortComments =  
+                this.comments["comments"]
+                .Where(isShort)
+                .Select(commentBody);
+
+            // return result
+            return shortComments.Count();
+        }
+
+        /// <summary>
+        /// Returns number of long comments (over 1000 characters), using lambda function assigned to
+        /// Action delegate.
+        /// </summary>
+        /// <returns>Integer number of long comments</returns>
+        public int getNumLongComments()
+        {
+            int counter = 0;
+            Action<JToken> checkLong = c =>
+            {
+                if (((String)c["body"]).Length >= 1000)
+                    counter++;
+            };
+
+            foreach (JToken c in this.comments["comments"])
+                checkLong(c);
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Returns the ith comment.
+        /// </summary>
+        /// <param name="i">Index of comment to return</param>
+        /// <returns>Comment body string</returns>
+        public String index(int i)
+        {
+            JToken comment = this.comments["comments"]
+                .Where((c, ci) => ci == i)
+                .First();
+            return (String)comment["body"];
+        }
+
+
+
     }
 }
--- LINQ/LINQApp/LINQApp/Program.cs
+++ LINQ/LINQApp/LINQApp/Program.cs
@@ -24,10 +24,30 @@ namespace LINQApp
             Console.WriteLine("Data for each comment includes: ");
             foreach (var cat in cs.getDataCategories())
             {
-                Console.WriteLine(cat);
+                Console.WriteLine("\t" + cat);
             }
+
+            // Search for the number of comments containing a particular term
+            String term = "egg";
+            Console.WriteLine("There are {0} comments containing the term \"{1}\"",
+                cs.stringSearch(term).Count(),
+                term);
+
+            // Search for the number of comments from a particular subreddit
+            String sub = "AskReddit";
+            Console.WriteLine("{0}% of comments come from the {1} subreddit.", 
+                ((float) cs.fromSub(sub).Count() / cs.getNumComments())*100, sub);
+
+            // Search for short comments
+            Console.WriteLine("There are {0} short comments.", cs.getNumShortComments());
+
+            // Search for long comments
+            Console.WriteLine("There are {0} long comments.", cs.getNumLongComments());
+
+            // Search for the nth comment
+            int index = 10000;
+            Console.WriteLine("Comment #{0} reads:\n{1}", index, cs.index(index));
             
-            Console.WriteLine(cs.stringSearch("egg").Count());
             // wait for user input
             Console.ReadKey();
         }
